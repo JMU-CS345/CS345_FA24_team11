7a24f2fd16c87605e489db28cae0fb13569bb464
Matt Wolffe <wolffemf@dukes.jmu.edu>
2024-10-26 09:41:47 -0400

    some common utils/helpers

---
+++ libraries/common.js
@@ -0,0 +1,67 @@
+/* Below are helpers that I constantly
+ * find myself having to rewrite when working
+ * in js. Add any helpers you may need here
+ * as long as there are no namespace problems
+ * 
+ */
+
+
+/**
+ * Slightly more reliable check on ability to parse
+ * a number from a string that doesn't require an ugly
+ * regex 
+ * 
+ * @param {*} str candidate string 
+ * @returns true if the string can be interpreted as a number,
+ *          false otherwise
+ */
+const isNumber = (str) => Number.isFinite(+str);
+
+
+/**
+ * Generates random integer in the range [min, max]
+ * 
+ * NOTE: this is not typesafe, that is, if you pass it some obj 
+ *       it will try to coerce said obj to a number.
+ *       In addition, if you pass it floating point values I believe
+ *       the distribution will not be uniform due to rounding bias
+ * 
+ * @param {*} min minimum possible value to generate (inclusive)
+ * @param {*} max maximum possible value to generate (inclusive)
+ * @returns a random integer between min and max, inclusive
+ */
+const randIntFromInclusiveRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+
+/**
+ * Generates random integer in the range [min, max)
+ * 
+ * NOTE: this is not typesafe, that is, if you pass it some obj 
+ *       it will try to coerce said obj to a number.
+ *       In addition, if you pass it floating point values I believe
+ *       the distribution will not be uniform due to rounding bias
+ * 
+ * @param {*} min minimum possible value to generate (inclusive)
+ * @param {*} max maximum possible value to generate (exclusive)
+ * @returns a random integer between min (inclusive) and max (exclusive)
+ */
+const randIntLeftInclusiveRange = (min, max) => Math.random() * (max - min) + min;
+
+/**
+ * This is essentially the same as randIntFromInclusiveRange, 
+ * but is more performant.
+ * 
+ * CAUTION: in using a bit shift (the `<< 0`), the operands are
+ *          treated as 32 bit 2's comp during computation, but 
+ *          the resulting value is not necessarily returned as such.
+ *          Use this if for some very bizarre reason one of the other
+ *          RNGs is slow.
+ *          Note that this applies for all bitwise/binary operators
+ * 
+ * @param {*} min minimum possible value to generate (inclusive)
+ * @param {*} max maximum possible value to generate (inclusive)
+ * @returns a random integer in the range [min, max || 2^32-1]
+ */
+const fastRandomIntegerFromRange = (min, max) => (Math.random() * (max - min + 1)) << 0;
+
+
