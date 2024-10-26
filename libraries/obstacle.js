5c5cd87d15fe7e59b927cf5f385817a31e47338f
Matt Wolffe <wolffemf@dukes.jmu.edu>
2024-10-26 14:22:37 -0400

    skeleton obstacle

---
+++ libraries/obstacle.js
@@ -0,0 +1,36 @@
+/** @todo @mfwolffe finish documenting */
+/** @todo @mfwolffe a bunch of other stuff */
+
+/**
+ * Obstacle model
+ *
+ * @class Obstacle
+ * @typedef {Obstacle}
+ * @classdesc
+ */
+class Obstacle {
+  // TODO some kind of assignment to xPos 
+  //      and yPos based on lane assignment
+  // 
+  constructor() {
+    this.lane = randIntLeftInclusiveRange(0, Game.NUMLANES);
+    this.speed = randIntFromInclusiveRange(1, Game.MAXSPDS[Game.difficulty]);
+    this.damage = randIntFromInclusiveRange(1, Game.MAXDMGS[Game.difficulty]);
+
+    this.xPos, this.yPos = this.initCoords();
+  }
+
+  getDirection() {
+    return this.lane < Game.NUMLANES / 2;
+  }
+
+  getHumanReadableDirection() {
+    return this.getDirection() ? "Left-Right" : "Right-Left";
+  }
+
+  initCoords() {
+    x = this.getDirection() ? Game.XSTARTLFT : Game.XSTARTRGT;
+    y = Game.LANECOORDS[this.lane];
+    return x, y;
+  }
+}
