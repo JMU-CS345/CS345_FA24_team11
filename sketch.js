c37bc0d8b08289d4a13bdf9d699628776f51de8f
Matt Wolffe <wolffemf@dukes.jmu.edu>
2024-10-26 14:35:55 -0400

    add boilerplate for using Game class

--- sketch.js
+++ sketch.js
@@ -1,12 +1,22 @@
-
+let p;
 
 function setup() {
-  createCanvas(720, 1280);
+  let g = new Game(2);
+  if (!Game.difficulty) throw new Error("Game not initialized");
+
+  /* ADD STUFF THAT DEPENDS ON `Game` AFTER HERE AS NEEDED */
+
+  p = new Player(100, 100, 20, 20, 20, 20);
+  createCanvas(Game.CANVAS.WIDTH, Game.CANVAS.HEIGHT);
 }
 
 function draw() {
   background(220);
   drawLandingPage();
+  
+  p.drawPlayer();
+  p.handlePlayerMovement();
+
 }
 
 // setup()
