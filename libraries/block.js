74fa4fbc36782e15c15fe6258a854440d9a137f1
Matt Wolffe <wolffemf@dukes.jmu.edu>
2024-10-28 22:34:40 -0400

    rudimentary block model

---
+++ libraries/block.js
@@ -0,0 +1,27 @@
+/** @todo @mfwolffe other fields */
+
+/**
+ * Game Block Model
+ *
+ * @class Block
+ * @typedef {Block}
+ * @author Matt Wolffe @mfwolffe, ** ADD YOURSELF HERE IF YOU EDIT ** 
+ * @classdesc Smallest game 'unit'. Arbitrarily chosen to be 20x20.
+ */
+class Block {
+  #coinPresent = false;
+
+  constructor(coinpresent) {
+    this.#coinPresent = coinpresent;
+  }
+
+  
+  /**
+   * Update this block's coin status
+   * 
+   * NOTE: The argument will be coerced to boolean.
+   *
+   * @param {*} change new value for coinPresent
+   */
+  updateCoin(change) { this.#coinPresent = Boolean(change); }
+}
