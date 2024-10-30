270a57447b6570eb8bf2c187443a1699425cd7aa
Matt Wolffe <wolffemf@dukes.jmu.edu>
2024-10-30 11:03:17 -0400

    more sensible var names

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
-let p;
+let player, game;
 
 function setup() {
-  let g = new Game(2);
+  game = new Game(2);
   if (!Game.difficulty) throw new Error("Game not initialized");
 
   /* ADD STUFF THAT DEPENDS ON `Game` AFTER HERE AS NEEDED */
 
-  p = new Player(100, 100, 20, 20, 20, 20);
+  player = new Player(100, 100, 20, 20, 20, 20);
   createCanvas(Game.CANVAS.WIDTH, Game.CANVAS.HEIGHT);
   console.log(Game.MAP.fetchMap());
 }
@@ -14,7 +14,6 @@ function setup() {
 function draw() {
   background(220);
   
-  p.drawPlayer();
-  p.handlePlayerMovement();
+  player.drawPlayer();
+  player.handlePlayerMovement();
 }
-console.log('hello world');
