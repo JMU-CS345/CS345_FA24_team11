57284928abe17dd056146f2e9ebc140f7df65480
Matt Wolffe <wolffemf@dukes.jmu.edu>
2024-11-10 10:02:26 -0500

    update sketch for usage instaead of menu.js

--- sketch.js
+++ sketch.js
@@ -2,19 +2,20 @@ let game, menu;
 
 
 /* ADD SPRITE VARIABLES HERE */
-let coinsprite;
+let coinSprite;
+let raccoonImg;
 let racoonSprite;
 
 function preload() {
   /* LOAD SPRITES HERE */
-  coinsprite = loadImage('assets/coin.gif');
+  coinSprite = loadImage('assets/coin.gif');
+  raccoonImg = loadImage('assets/raccoon.jpg');
   racoonSprite = loadImage('assets/raccoon.jpg');
-
 }
 
 function setup() {
   /** @todo @mfwolffe don't forget about difficulty */
-  game = new Game(2);
+  game = new Game(1, 1280, 720);
   menu = new Menu(0.5);
 
   // if you see the error thrown below 
@@ -24,14 +25,51 @@ function setup() {
 
   /* ADD STUFF THAT DEPENDS ON `Game` BELOW HERE AS NEEDED */
   createCanvas(Game.CANVAS.WIDTH, Game.CANVAS.HEIGHT);
-  console.log(Game.MAP.fetchMap());
+  textAlign(CENTER, CENTER);
 }
 
+function mousePressed() {
+  switch(currentScreen) {
+    case 'main':
+      console.log("here");
+      GameMenu.handleMainMenuClick();
+      break;
+    case 'store':
+      handleStoreMenuClick();
+      break;
+    case 'cosmetics':
+    case 'powerups':
+      handleSubmenuClick();
+      break;
+    case 'inventory':
+      handleInventoryClick();
+      break;
+    case 'casino':
+      // handleCasinoMenuClick();
+      
+      break;
+    case 'game':
+      Game.handleGameClick();
+      break;
+    }
+}
+
+
 function draw() {
-  Game.MAINMENU.colorizeBackground();
-  
-  Game.player.drawPlayer();
-  Game.player.handlePlayerMovement();
-  
-  Game.MAP.renderCoins();
+  /** @todo @mfwolffe don't call this here */
+  Game.startGame();
+
+  switch(currentScreen) {
+    case 'game':        Game.drawGame();                    break;
+    case 'main':        GameMenu.drawMenu();                break;
+    case 'store':       drawStoreMenu();                    break;
+    case 'inventory':   drawInventory();                    break;
+    case 'inventory':   !blackJackInit && setupBlackJack(); break;
+    case 'powerups':
+    case 'cosmetics':   drawSubmenu(currentScreen === 'cosmetics' ? cosmetics : powerUps);
+      break;
+    default: 
+      console.log("BAD SCENE");
+      break;
+  }
 }
