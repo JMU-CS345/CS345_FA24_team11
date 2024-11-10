eee3ad4f474aefac294520d5308c4bd697cc46ab
Matt Wolffe <wolffemf@dukes.jmu.edu>
2024-11-10 10:09:34 -0500

    change approach to obstacle

--- libraries/obstacle.js
+++ libraries/obstacle.js
@@ -1,3 +1,4 @@
+/** @todo @mfwolffe bind at runtime */
 /** @todo @mfwolffe finish documenting */
 /** @todo @mfwolffe a bunch of other stuff */
 
@@ -10,28 +11,24 @@
  * @classdesc
  */
 class Obstacle {
-  // TODO some kind of assignment to xPos 
+  #xPos;
+  #yPos;
+
+  // TODO some kind of assignment to xPos
   //      and yPos based on lane assignment
   // 
-  constructor() {
+  constructor(xPos=null, yPos=null) {
     this.lane = randIntLeftInclusiveRange(0, Game.NUMLANES);
-    this.speed = randIntFromInclusiveRange(1, Game.MAXSPDS[Game.difficulty]);
-    this.damage = randIntFromInclusiveRange(1, Game.MAXDMGS[Game.difficulty]);
 
-    this.xPos, this.yPos = this.initCoords();
-  }
+    /** @see new Vehicle class */
+    // this.speed = randIntFromInclusiveRange(1, Game.MAXSPDS[Game.difficulty]);
+    // this.damage = randIntFromInclusiveRange(1, Game.MAXDMGS[Game.difficulty]);
 
-  getDirection() {
-    return this.lane < Game.NUMLANES / 2;
-  }
-
-  getHumanReadableDirection() {
-    return this.getDirection() ? "Left-Right" : "Right-Left";
-  }
-
-  initCoords() {
-    x = this.getDirection() ? Game.XSTARTLFT : Game.XSTARTRGT;
-    y = Game.LANECOORDS[this.lane];
-    return x, y;
+    if (!xPos && !yPos)
+      this.#xPos, this.#yPos = this.initCoords();
+    else {
+      this.#xPos = xPos;
+      this.#yPos = yPos;
+    }
   }
 }
