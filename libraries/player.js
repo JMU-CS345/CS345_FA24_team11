2126894bcaba9c93aeea6b0a7f55fcff6f2fff90
Matt Wolffe <wolffemf@dukes.jmu.edu>
2024-11-11 09:11:36 -0500

    spritesheet needs full implementation

--- libraries/player.js
+++ libraries/player.js
@@ -12,7 +12,6 @@ const DEFAULTCURRENCY = 100;
  * @author Matt Wolffe @mfwolffe, @stroudw ** ADD YOURSELF HERE IF YOU EDIT ** 
  * @classdesc
  */
-
 class Player {
   constructor(xPos=Game.STARTX,
     yPos=Game.STARTY,
@@ -22,42 +21,53 @@ class Player {
     currency=DEFAULTCURRENCY) {
       this.xPos = xPos;
       this.yPos = yPos;
-      this.xSpeed = xSpeed;
-      this.ySpeed = ySpeed;
+      this.xSpeed = Game.BLOCKSIZE;
+      this.ySpeed = Game.BLOCKSIZE;
       this.health = health;
       this.currency = currency;
-      
+
+      this.isMoving = false;
+      this.facing = 'up';
+
       // TODO @mfwolffe regroup on inventory DS choice
       this.inventory = [];
   }
 
   handlePlayerMovement() {
-    this.isMoving = false;
-
-    if (keyIsDown(DOWN_ARROW)) {
-      this.updatePlayerYPos(this.ySpeed);
-      this.facing = 'down';
-      this.isMoving = true;
-    }
-    if (keyIsDown(UP_ARROW)) {
-      this.updatePlayerYPos(-(this.ySpeed));
-      this.facing = 'up';
-      this.isMoving = true;
-    }
-    if (keyIsDown(RIGHT_ARROW)) {
-      this.updatePlayerXPos(this.xSpeed);
-      this.facing = 'right';
-      this.isMoving = true;
-    }
-    if (keyIsDown(LEFT_ARROW)) {
-      this.updatePlayerXPos(-(this.xSpeed));
-      this.facing = 'left';
-      this.isMoving = true;
-    }
-
-    // Always update animation, whether moving or idle
-    this.updateAnimation();
+    keyIsDown(DOWN_ARROW) && this.updatePlayerYPos(this.ySpeed);
+    keyIsDown(UP_ARROW) && this.updatePlayerYPos(-(this.ySpeed));
+    keyIsDown(RIGHT_ARROW) && this.updatePlayerXPos(this.xSpeed);
+    keyIsDown(LEFT_ARROW) && this.updatePlayerXPos(-(this.xSpeed));
   }
+
+
+  // handlePlayerMovement() {
+  //   this.isMoving = false;
+
+  //   if (keyIsDown(DOWN_ARROW)) {
+  //     this.updatePlayerYPos(this.ySpeed);
+  //     this.facing = 'down';
+  //     this.isMoving = true;
+  //   }
+  //   if (keyIsDown(UP_ARROW)) {
+  //     this.updatePlayerYPos(-(this.ySpeed));
+  //     this.facing = 'up';
+  //     this.isMoving = true;
+  //   }
+  //   if (keyIsDown(RIGHT_ARROW)) {
+  //     this.updatePlayerXPos(this.xSpeed);
+  //     this.facing = 'right';
+  //     this.isMoving = true;
+  //   }
+  //   if (keyIsDown(LEFT_ARROW)) {
+  //     this.updatePlayerXPos(-(this.xSpeed));
+  //     this.facing = 'left';
+  //     this.isMoving = true;
+  //   }
+
+  //   // Always update animation, whether moving or idle
+  //   this.updateAnimation();
+  // }
 
   updateAnimation() {
     this.frameCount++;
@@ -74,36 +84,40 @@ class Player {
 
   drawPlayer() {
     // Calculate source rectangle from sprite sheet
-    const animation = this.animations[this.facing];
-    const sx = (this.currentFrame + animation.startFrame) * this.spriteWidth;
-    const sy = animation.row * this.spriteHeight;
+    // const animation = this.animations[this.facing];
+    // const sx = (this.currentFrame + animation.startFrame) * this.spriteWidth;
+    // const sy = animation.row * this.spriteHeight;
     
-    push();  // Save current rendering state
-    noSmooth();  // Ensure pixel-perfect rendering
+    // push();  // Save current rendering state
+    // noSmooth();  // Ensure pixel-perfect rendering
     
-    // Draw the current frame
-    image(
-      this.sprite,
-      this.xPos + (this.spriteWidth * 1.5) / 2,  // Center the sprite horizontally
-      this.yPos + (this.spriteHeight * 1.5) / 2, // Center the sprite vertically
-      this.spriteWidth * 3,
-      this.spriteHeight * 3,
-      sx,
-      sy,
-      this.spriteWidth,
-      this.spriteHeight
-    );
+    // // Draw the current frame
+    // image(
+    //   this.sprite,
+    //   this.xPos + (this.spriteWidth * 1.5) / 2,  // Center the sprite horizontally
+    //   this.yPos + (this.spriteHeight * 1.5) / 2, // Center the sprite vertically
+    //   this.spriteWidth * 3,
+    //   this.spriteHeight * 3,
+    //   sx,
+    //   sy,
+    //   this.spriteWidth,
+    //   this.spriteHeight
+    // );
     
-    pop();  // Restore previous rendering state
+    // pop();  // Restore previous rendering state
+
+    fill("blue");
+    rect(this.xPos, this.yPos, 20, 20);
   }
 
-  // ... keep other existing methods unchanged ...
   updatePlayerXPos(change) {
-    this.xPos += change;
+    let cndt = this.xPos + change;
+    if (cndt < Game.CANVAS.WIDTH && cndt >= 0) this.xPos = cndt;
   }
 
   updatePlayerYPos(change) {
-    this.yPos += change;
+    let cndt = this.yPos + change;
+    if (cndt < Game.CANVAS.HEIGHT && cndt >= 0) this.yPos = cndt;
   }
 
   setXSpeed(change) {
@@ -120,5 +134,24 @@ class Player {
 
   updatePlayerCurrency(change) {
     this.currency += change;
+  }
+
+  /** 
+   * Concise collision check idea thanks to Alice MC (@alicmc)
+   * My original implementation was cruffed as hell.
+   * 
+   */
+  checkCollectCoin() {
+    let i = Math.floor(this.xPos / Game.BLOCKSIZE);
+    let j = Math.floor(this.yPos / Game.BLOCKSIZE);
+
+    let block;
+
+    return ((block = Game.MAP.fetchBlock(i, j)).checkCoin()) ? block : null;
+  }
+
+  collectCoin(block) {
+    block.updateCoin(false);
+    GameMap.renderGrid();
   }
 }
