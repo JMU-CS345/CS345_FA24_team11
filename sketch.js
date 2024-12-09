ecd1faec11df152a4929bc8673b33fbdcbf40ce8
stroudw <stroudwj@dukes.jmu.edu>
2024-12-09 18:06:01 -0500

    simple color changes

--- sketch.js
+++ sketch.js
@@ -1,6 +1,4 @@
 let game, menu;
-
-
 /* ADD SPRITE VARIABLES HERE */
 let coinSprite;
 let raccoonImg;
@@ -8,93 +6,87 @@ let racoonSprite;
 let roadBotImg, roadTopImg, sidewalkImg, grassImg, waterImg, carpetImg;
 
 function preload() {
-  /* LOAD SPRITES HERE */
-  coinSprite = loadImage('assets/coin.gif');
-  raccoonImg = loadImage('assets/raccoon.jpg');
-  racoonSprite = loadImage('assets/raccoon.jpg');
-  grassImg = loadImage('assets/mapGraphics/grass.png');
-  waterImg = loadImage('assets/mapGraphics/water2.png');
-  bridgeImg = loadImage('assets/mapGraphics/bridge.png');
-  roadBotImg = loadImage('assets/mapGraphics/roadBot.png');
-  roadTopImg = loadImage('assets/mapGraphics/roadTop.png');
-  carpetImg = loadImage('assets/mapGraphics/redCarpet.png');
-  sidewalkImg = loadImage('assets/mapGraphics/sidewalk2.png');
-  // casinoImg = loadImage('assets/mapGraphics/casino.png');
-
-  preloadCasino();
-  preloadSlots();
+    /* LOAD SPRITES HERE */
+    coinSprite = loadImage('assets/coin.gif');
+    raccoonImg = loadImage('assets/raccoon.jpg');
+    racoonSprite = loadImage('assets/rSprite.png');
+    grassImg = loadImage('assets/mapGraphics/grass.png');
+    waterImg = loadImage('assets/mapGraphics/water2.png');
+    bridgeImg = loadImage('assets/mapGraphics/bridge.png');
+    roadBotImg = loadImage('assets/mapGraphics/roadBot.png');
+    roadTopImg = loadImage('assets/mapGraphics/roadTop.png');
+    carpetImg = loadImage('assets/mapGraphics/redCarpet.png');
+    sidewalkImg = loadImage('assets/mapGraphics/sidewalk2.png');
+    // casinoImg = loadImage('assets/mapGraphics/casino.png');
+    preloadCasino();
+    preloadSlots();
+    
+    // Initialize menu sprite
+    GameMenu.initializeSprite();
 }
 
-function setup() {
-  /** @todo @mfwolffe don't forget about difficulty */
-  game = new Game(1, 1280, 720);
-  menu = new Menu(0.5);
+async function setup() {
+    /** @todo @mfwolffe don't forget about difficulty */
+    game = new Game(1, 1280, 720);
+    menu = new Menu(0.5);
 
-  // if you see the error thrown below
-  // in console, you are likely just
-  // missing an instance of a game.
-  if (!Game.difficulty) throw new Error("Game not initialized");
+    // if you see the error thrown below
+    // in console, you are likely just
+    // missing an instance of a game.
+    if (!Game.difficulty) throw new Error("Game not initialized");
 
-  /* ADD STUFF THAT DEPENDS ON `Game` BELOW HERE AS NEEDED */
-  createCanvas(Game.CANVAS.WIDTH, Game.CANVAS.HEIGHT);
-  textAlign(CENTER, CENTER);
+    /* ADD STUFF THAT DEPENDS ON `Game` BELOW HERE AS NEEDED */
+    createCanvas(Game.CANVAS.WIDTH, Game.CANVAS.HEIGHT);
+    textAlign(CENTER, CENTER);
 
-  // console.log(`PLAYER DFAULT CURRENCY: ${Game.player.currency}`);
-
-  setupTileTypes();
-  setupAggregateTypes();
-  initMap();
+    setupTileTypes();
+    setupAggregateTypes();
+    initMap();
 }
 
 function mousePressed() {
-  switch(currentScreen) {
-    case 'main':      GameMenu.handleMainMenuClick();     break;
-    case 'store':     StoreMenu.handleStoreMenuClick();   break;
-    case 'cosmetics':
-    case 'powerups':  StoreMenu.handleSubmenuClick();     break;
-    case 'inventory': StoreMenu.handleInventoryClick();   break;
-    case 'casino':    handleCasinoMenuClick();            break;
-    case 'game':      Game.handleGameClick();             break;
-    case 'difficulty' : handleDifficultyClick();          break;
+    switch(currentScreen) {
+        case 'main': GameMenu.handleMainMenuClick(); break;
+        case 'store': StoreMenu.handleStoreMenuClick(); break;
+        case 'cosmetics':
+        case 'powerups': StoreMenu.handleSubmenuClick(); break;
+        case 'inventory': StoreMenu.handleInventoryClick(); break;
+        case 'casino': handleCasinoMenuClick(); break;
+        case 'game': Game.handleGameClick(); break;
+        case 'difficulty': handleDifficultyClick(); break;
     }
 }
 
 function keyReleased() {
-  if (currentScreen == 'game') {
-    Game.player.keyReleased();
-  }
+    if (currentScreen == 'game') {
+        Game.player.keyReleased();
+    }
 }
 
 function keyPressed() {
-  if (currentScreen == 'game') {
-    CoinMultiplier.handleCoinKeyPress(key);
-    DashPowerUp.handleDashKeyPress(key);
-  }
-
-  if (currentCasinoScreen == 'blackjack') keyPressedBlackJack(keyCode);
-
-  if (currentScreen === 'main' && key === ' ') Game.describeGame();
+    if (currentScreen == 'game') {
+        CoinMultiplier.handleCoinKeyPress(key);
+        DashPowerUp.handleDashKeyPress(key);
+    }
+    if (currentCasinoScreen == 'blackjack') keyPressedBlackJack(keyCode);
+    if (currentScreen === 'main' && key === ' ') Game.describeGame();
 }
 
 function draw() {
-  /** @todo @mfwolffe don't call this here */
-  background("teal");
-
-  switch(currentScreen) {
-    case 'game':        Game.drawGame();                      break;
-    case 'difficulty':  drawDifficultyMenu();                 break;
-    case 'main':        GameMenu.drawMenu();                  break;
-    case 'store':       StoreMenu.drawStoreMenu();            break;
-    case 'casino':      setupCasinoUI(); drawCasino();        break;
-    case 'inventory':   StoreMenu.drawInventory();            break;
-    case 'powerups':    /** @todo @mfwolffe don't do below like that, inject it */
-    case 'cosmetics':   StoreMenu.drawSubmenu(currentScreen === 'cosmetics' ? StoreMenu.cosmetics : StoreMenu.powerUps);
-      break;
-    default:
-      console.log("BAD SCENE");
-      break;
-  }
-
-  // let block;
-  // (block = Game.player.checkCollectCoin()) && Game.player.collectCoin(block);
+    /** @todo @mfwolffe don't call this here */
+    background("teal");
+    switch(currentScreen) {
+        case 'game': Game.drawGame(); break;
+        case 'difficulty': drawDifficultyMenu(); break;
+        case 'main': GameMenu.drawMenu(); break;
+        case 'store': StoreMenu.drawStoreMenu(); break;
+        case 'casino': setupCasinoUI(); drawCasino(); break;
+        case 'inventory': StoreMenu.drawInventory(); break;
+        case 'powerups': /** @todo @mfwolffe don't do below like that, inject it */
+        case 'cosmetics': StoreMenu.drawSubmenu(currentScreen === 'cosmetics' ? StoreMenu.cosmetics : StoreMenu.powerUps);
+            break;
+        default:
+            console.log("BAD SCENE");
+            break;
+    }
 }
